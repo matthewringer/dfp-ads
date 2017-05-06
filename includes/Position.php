ba3885a1fac78f1e2ea0a83074181ec1db0ca479
supineunicorn <mringer@gmail.com>
2017-05-06 01:37:14 -0400

    fix test

--- includes/Position.php
+++ includes/Position.php
@@ -210,9 +210,7 @@ class Position {
 		     class="<?php _e( $this->position_tag, 'dfp-ads' ); ?> <?php _e( $this->ad_name, 'dfp-ads' ); ?> <?php _e( $this->position_class, 'dfp-ads' ); ?>" data-adpos = "<?php _e( $this->position_tag, 'dfp-ads' ); ?>">
 			<script type='text/javascript'>
 				googletag.cmd.push(function () {
-					// create a new slot from the
 					dfp_ads.display_ad_position('<?php _e( $this->position_tag.'-'.$mSecs, 'dfp-ads'); ?>');
-					//googletag.display('<?php _e( $this->position_tag.$mSecs, 'dfp-ads'); ?>');
 				});
 			</script>
 		</div>
