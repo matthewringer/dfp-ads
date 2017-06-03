62f574a072d7e821a380bfd67ed6a55ffbae568d
supineunicorn <mringer@gmail.com>
2017-06-03 14:30:59 -0400

    fix js linting and minification

--- assets/js/dfp-ads.js
+++ assets/js/dfp-ads.js
@@ -3,27 +3,11 @@
  *
  **/
 
-var dfp_ads = function() {
+var dfp_ads =  (function() {
 
   // Object from Ajax
   var dfp_ad_data = dfp_ad_object[0],
     acct_id = dfp_ad_data.account_id;
-
-  /** 
-   * Create a new ad position based using the ad unit info as a template.
-   * 
-   * @param {string} id - div id of the ad postion wrapper
-  */
-  function display_ad_position(id){
-      let ad_slot = document.querySelector(`#${id}`);
-      let ad_pos = ad_slot.dataset['adpos'];
-      let pos_template = dfp_ad_data.positions.find(p=>p.position_tag == ad_pos);
-      let new_pos = Object.create(pos_template);
-      new_pos.position_tag = id;
-      let position = define_ad_slot(new_pos);
-      googletag.pubads().refresh(position);
-      googletag.display(id);
-  };
 
   /**
    * Loads Ad Position
@@ -32,28 +16,16 @@ var dfp_ads = function() {
    */
   function define_ad_slot(position) {
 
-    let slot = googletag.defineSlot(
+    var slot = googletag.defineSlot(
       acct_id + position.ad_name,
       position.sizes,
       position.position_tag
     );
 
     if(position.size_mapping !== undefined && position.size_mapping !== null) {
-      let mapping = JSON.parse(position.size_mapping);
-      //console.log( mapping );
-      // let mapping = googletag.sizeMapping().
-      // addSize([970, 0], [970, 250]).
-      // addSize([0, 0], [300, 250]).
-      // build();
-
+      var mapping = JSON.parse(position.size_mapping);
       slot.defineSizeMapping(mapping);
-      
-      //addSize([1024, 768], [970, 250]).
-      //addSize([0, 0], [970, 250]).
-
       $(window).resize({ad_slot: slot}, function(e){
-        console.log('resized');
-        console.log(e.data);
         googletag.pubads().refresh([e.data.ad_slot])
       });
     }
@@ -70,6 +42,23 @@ var dfp_ads = function() {
     return slot;
   }
 
+  /** 
+   * Create a new ad position based using the ad unit info as a template.
+   * 
+   * @param {string} id - div id of the ad postion wrapper
+  */
+  function display_ad_position(id){
+      'use strict';
+      var ad_slot = document.querySelector('#'+id);
+      var ad_pos = ad_slot.dataset.adpos;
+      var pos_template = dfp_ad_data.positions.find(function(p){return p.position_tag === ad_pos;});
+      var new_pos = Object.create(pos_template);
+      new_pos.position_tag = id;
+      var position = define_ad_slot(new_pos);
+      googletag.pubads().refresh(position);
+      googletag.display(id);
+  }
+
   /**
    * Sets Page level targeting
    * @param {object} targeting
@@ -82,7 +71,7 @@ var dfp_ads = function() {
   }
 
   return { 'display_ad_position' : display_ad_position };
-} ();
+}());
 
 /**
  * Ad Position Creation
